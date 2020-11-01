5173f8264c236fc996a9e982108b076ff463aea8
JeanExtreme002 <jeangamerextreme@gmail.com>
2020-11-01 16:58:23 -0200

    More dynamic configuration and formatted code.

--- GradientFrame.py
+++ GradientFrame.py
@@ -1,102 +1,103 @@
+__author__ = "Jean Loui Bernard Silva de Jesus"
 
 from tkinter import Canvas
 
 class GradientFrame(Canvas):
+    
     """
-    Classe para criar um frame com cores em degradê
+    Widget with gradient colors.
     """
 
     __tag = "GradientFrame"
-
-    hex_format = "#%04x%04x%04x"
+    __hex_format = "#%04x%04x%04x"
+    
     top2bottom = 1
     left2right = 2
 
+    def __init__(self, parent, colors = ("red", "black"), direction = left2right, **kw):
 
-    def __init__(self,parent,geometry,colors=("red","black"),direction=left2right,**kw):
+        # Caso o usuário não tenha configurado uma geometria, será definido uma geometria padrão.
+        kw["height"] = kw.get("height", 200)
+        kw["width"] = kw.get("width", 200)
         
-        #Invoca o método construtor do Canvas
-        Canvas.__init__(self,parent,width=geometry[0],height=geometry[1],**kw)
+        # Chama o método construtor do Canvas.
+        super().__init__(parent, **kw)
 
-        #Instância os parâmetros
-        self.__geometry = geometry
+        # Instancia os parâmetros.
+        self.__geometry = [kw["width"], kw["height"]]
         self.__colors = colors
         self.__direction = direction
 
-        #Desenha um degradê no canvas
-        self.__drawGradient()
-
+        # Desenha o degradê no Canvas.
+        self.__draw_gradient()
         
-    def __drawGradient(self):
+    def __draw_gradient(self):
         
         """
-        Método para pintar o canvas com cores em degradê
+        Paint the Canvas with gradient colors.
         """
 
-        #Deleta o degradê do canvas, caso exista
+        # Apaga o degradê do Canvas.
         self.delete(self.__tag)
 
-        #Recebe o limite de largura
+        # Recebe o limite de largura.
         limit = self.__geometry[0] if self.__direction == self.left2right else self.__geometry[1]
        
-        #Recebe os valores RGB das cores
-        red1,green1,blue1 = self.winfo_rgb(self.__colors[0])
-        red2,green2,blue2 = self.winfo_rgb(self.__colors[1])
+        # Recebe os valores RGB das cores.
+        red1, green1, blue1 = self.winfo_rgb(self.__colors[0])
+        red2, green2, blue2 = self.winfo_rgb(self.__colors[1])
 
-        #Calcula os valores RGB de acréscimo de cores (Ex: while red1 != red2: red1 += r_ratio ) 
-        #dividindo o mesmo pelo limite de largura.
-        r_ratio = float(red2 - red1) / limit
-        g_ratio = float(green2 - green1) / limit
-        b_ratio = float(blue2 - blue1) / limit
+        # Calcula os valores RGB de acréscimo de cores (Ex: while red1 != red2: red1 += r_ratio) 
+        # dividindo o mesmo pelo limite de largura.
+        r_ratio = (red2 - red1) / limit
+        g_ratio = (green2 - green1) / limit
+        b_ratio = (blue2 - blue1) / limit
 
         for pixel in range(limit):
             
-            #Calcula a cor em formato RGB
-            red = int( red1 + ( r_ratio * pixel ) )
-            green = int( green1 + ( g_ratio * pixel ) )
-            blue = int( blue1 + ( b_ratio * pixel ) )
+            # Calcula a cor em formato RGB.
+            red = int(red1 + (r_ratio * pixel))
+            green = int(green1 + (g_ratio * pixel))
+            blue = int(blue1 + (b_ratio * pixel))
 
-            #Transforma a cor de RGB em Hex
-            color = self.hex_format % (red,green,blue)
+            # Converte a cor de RGB para Hex.
+            color = self.__hex_format % (red, green, blue)
 
-            #Define a posição (x1,y1,x2,y2) no objeto
+            # Define as posições (x1, y1, x2, y2) do objeto.
             x1 = pixel if self.__direction == self.left2right else 0
             y1 = 0 if self.__direction == self.left2right else pixel
             x2 = pixel if self.__direction == self.left2right else self.__geometry[0]
             y2 = self.__geometry[1] if self.__direction == self.left2right else pixel
 
-            #Cria o retângulo no canvas
-            self.create_line(x1,y1,x2,y2 , tag=self.__tag , fill=color)
+            # Cria uma linha no canvas com uma das cores do degradê.
+            self.create_line(x1, y1, x2, y2, tag = self.__tag, fill = color)
 
+        # Coloca o degradê atrás de todos os elementos do Canvas.
+        self.tag_lower(self.__tag)
 
-    def setColors(self,colors):
-        """
-        Método para definir as cores que serão usadas no degradê
-        """
+    def config(self, cnf = None, **kw):
 
-        self.__colors = colors
-        self.__drawGradient()
+        # Configura as cores do degradê.
+        if "colors" in kw and len(kw["colors"]) > 1:
+            self.__colors = kw.pop("colors")
 
+        # Configura a direção do degradê.
+        if "direction" in kw and kw["direction"] in (self.left2right, self.top2bottom):
+            self.__direction = kw.pop("direction")
 
-    def setDirection(self,direction):
-        """
-        Método para definir a direção do degradê
-        """
+        # Configura a altura do degradê.  
+        if "height" in kw:
+            self.__geometry[1] = kw["height"]
 
-        if direction in (self.left2right,self.top2bottom):
-            self.__direction = direction
-            self.__drawGradient()
-        else:
-            raise ValueError('The "direction" parameter must be self.left2right or self.top2bottom')
-        
+        # Configura a largura do degradê.
+        if "width" in kw:
+            self.__geometry[0] = kw["width"]
 
-    def setGeometry(self,geometry):
-        """
-        Método para definir as dimensões do frame
-        """
+        # Configura o Canvas e desenha o degradê.
+        super().config(**kw)
+        self.__draw_gradient()
 
-        self.config(width=geometry[0],height=geometry[1])
-        self.__geometry = geometry
-        self.__drawGradient()
+    def configure(self, cnf = None, **kw):
+        self.config(cnf, **kw)
  
 
